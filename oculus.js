16c87fb527616637f3bf755a9974ec368349d026
Rob <rdaly1490@gmail.com>
2015-12-23 20:55:41 -0500

    made code able to search multiple twitter accounts

--- oculus.js
+++ oculus.js
@@ -20,47 +20,54 @@ var T = new Twit({
 	access_token_secret: twitterKeys.accessTokenSecret
 });
 
-var matchingTweet = '';
+var matchingTweets = [];
 
-// user's twitter handle and number of posts to check
-var options = { screen_name: 'oculus',
-                count: 1 };
+// Array of twitter handles to check
+var usersToCheck = ['oculus', 'PalmerLuckey', 'brendaniribe', 'natemitchell'];
 
-// Function to check user's most recent tweet for certain keywords
-function getRecentTweets() {
-	T.get('statuses/user_timeline', options , function(err, tweet) {
-		if (err) {
-			console.log(err);
-		}
-		// T.get returns array of length one, so grab the array item
-		var latestTweet = tweet[0];
-
-		var tweetCased = latestTweet.text.toLowerCase();
-
-		// If any of these words appear in the tweet, evaluates to true
-		if (tweetCased.indexOf('preorder') > -1 || 
-			tweetCased.indexOf('pre-order') > -1 ||
-			tweetCased.indexOf('buy') > -1 ||
-			tweetCased.indexOf('launch') > -1) {
-
-			// Check if we've seen this tweet before and if so do not execute text message again
-			if (latestTweet.text !== matchingTweet) {
-				client.messages.create({
-					body: latestTweet.text,
-					to: phoneNumbers.myNumber,
-					from: phoneNumbers.twilioNumber
-				}, function(err) {
-					console.log(err);
-				});
+function checkTwitterAccounts() {
+	usersToCheck.forEach(function(user, index) {
+		// Current user's twitter handle and number of posts to check
+		var options = { screen_name: user,
+	                count: 1 };
+		T.get('statuses/user_timeline', options , function(err, tweet) {
+			if (err) {
+				console.log(err);
 			}
-			matchingTweet = latestTweet.text;
-		}
+
+			// T.get returns array of length one, so grab the array item
+			var latestTweet = tweet[0];
+
+			var tweetCased = latestTweet.text.toLowerCase();
+
+			// If any of these words appear in the tweet, evaluates to true
+			if (tweetCased.indexOf('preorder') > -1 || 
+				tweetCased.indexOf('pre-order') > -1 ||
+				tweetCased.indexOf('buy') > -1 ||
+				tweetCased.indexOf('launch') > -1) {
+
+				// Check if this tweet is already in the array at the twitter user's index
+				// so we do not execute text message again
+				if (latestTweet.text !== matchingTweets[index]) {
+					// If this is a new tweet for the user, set equal to that index in the array
+					matchingTweets[index] = latestTweet.text;
+					client.messages.create({
+						body: latestTweet.text,
+						to: phoneNumbers.myNumber,
+						from: phoneNumbers.twilioNumber
+					}, function(err) {
+						console.log(err);
+					});
+				}
+			}
+		});
 	});
-};
+}
 
-// Checks user's tweets every 15 seconds for a match
-setInterval(getRecentTweets, 15000);
+// Twitter allows 180 requests in a 15 min window, so interval is 5 sec * number of accounts checked
+var refreshInterval = 5000 * usersToCheck.length;
 
+setInterval(checkTwitterAccounts, refreshInterval);
 
 
 
